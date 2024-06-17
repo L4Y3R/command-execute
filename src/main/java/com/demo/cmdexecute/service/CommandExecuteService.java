1f98280e814ff0a680d74cc74f37665870f6a02d
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2024-06-17 16:42:06 +0530

    prints the offset as well

--- src/main/java/com/demo/cmdexecute/service/CommandExecuteService.java
+++ src/main/java/com/demo/cmdexecute/service/CommandExecuteService.java
@@ -1,13 +1,16 @@
 package com.demo.cmdexecute.service;
 
 import com.demo.cmdexecute.DTO.CommandDTO;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommandExecuteService {
     @KafkaListener(topics = "command_topic", groupId = "command_execute_group")
-    public void listen(CommandDTO message) {
-        System.out.println("Received Message in group command_execute_group: " + message);
+    public void listen(ConsumerRecord<String, CommandDTO> record) {
+        CommandDTO message = record.value();
+        long offset = record.offset();
+        System.out.println("Received Message in group command_execute_group: " + message + " Offset [" + offset + "]");
     }
 }
